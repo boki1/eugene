fd706270b593c304ad172087722ec72768346cf5
Kristiyan Stoimenov <kristoimenov@gmail.com>
2021-12-11 20:11:00 +0200

    Btree: Test operations
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- src/core/storage/btree/Btree.cpp
+++ src/core/storage/btree/Btree.cpp
@@ -1,13 +1,19 @@
-#include "catch2/catch.hpp"
+#include <iostream>
+#include <map>
+
+#include <catch2/catch.hpp>
+#include <fmt/core.h>
 
 #include <core/storage/Storage.h>
 #include <core/storage/btree/Btree.h>
-
-#include <iostream>
-#include <ranges>
-using namespace std::ranges::views;
-
 using namespace internal::storage::btree;
+
+auto item() {
+	static std::random_device dev;
+	static std::mt19937 rng(dev());
+	static std::uniform_int_distribution<std::mt19937::result_type> dist(1, 10000000);
+	return dist(rng);
+}
 
 TEST_CASE("Btree operations", "[btree]") {
 	Btree bpt("/tmp/eu-btree-pgcache_name");
@@ -19,4 +25,17 @@ TEST_CASE("Btree operations", "[btree]") {
 	REQUIRE(bpt.root().leaf().m_vals[0] == 1);
 
 	REQUIRE(bpt.get(42).value() == 1);
+
+	std::map<Config::Key, Config::Val> backup;
+	while (backup.size() != 200000) {
+		auto key = item();
+		auto val = key;//item();
+		bpt.put(key, val);
+		backup.emplace(key, val);
+	}
+
+	int i = 1; for (const auto &[key, val] : backup) {
+		REQUIRE(bpt.get(key).value() == val);
+		fmt::print("-- {} -> Key '{}' found mapped to '{}'\n", i++, key, val);
+	}
 }
