28ad1e57e30e3e679856388f1f9f83b2ee5a72dd
Tinchev <st8tinchev@gmail.com>
2022-02-17 12:15:50 +0200

    Add new logger logic with `rotating_logger_mt` which uses rotating algorithm and renaming algorithm; also documentation

--- src/core/Logger.h
+++ src/core/Logger.h
@@ -1,21 +1,29 @@
 #pragma once
 
 #include <memory>
-#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/spdlog.h>
 
+/// @brief Singleton and thread-safe
 class Logger {
 public:
+	/// @brief Singleton instance
+	/// @return Logger instance
 	static spdlog::logger &the() {
 		static Logger instance;
 		return *instance._logger;
 	}
 private:
-	std::shared_ptr<spdlog::logger> _logger = spdlog::basic_logger_mt("logger",
-	                                                                  "logs.txt", false);
-	Logger() {
-		spdlog::flush_every(std::chrono::seconds(5));
-	};
+	long max_size = 1048576 * 5; //!< 5MB max size of the log file
+	long max_files = 1; //!< 2 files to keep in rotation
+
+	std::shared_ptr<spdlog::logger> _logger =
+		spdlog::rotating_logger_mt(
+			"logger", "logs.txt", max_size, max_files
+		); //!< logger instance
+
+	/// @brief Constructor
+	Logger() = default;
 public:
 	Logger(Logger const &) = delete;
 	void operator=(Logger const &) = delete;
