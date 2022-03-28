8b1c560cd3ed3301d3d588d850b9216d3c3ebf60
Tinchev <st8tinchev@gmail.com>
2022-03-28 11:51:36 +0300

    Small documentation updates

--- src/core/storage/compression/benchmarks/Shared.h
+++ src/core/storage/compression/benchmarks/Shared.h
@@ -6,11 +6,14 @@
 #include <core/storage/compression/Compressor.h>
 #include <core/storage/compression/Decompressor.h>
 
+constexpr std::string_view file_name = "test.txt"; //!< File name to be used for
+//!< compression/decompression benchmarking.
 
-constexpr std::string_view file_name = "test.txt"; //!< File name to be used for compression/decompression benchmarking.
 constexpr std::string_view compressed_file_name = "compressed"; //!< File name to be used for compressed file.
 
-/// @brief Static function that will clean up the files created by the compression/decompression benchmarks.
+/// @brief Static function that will clean up the files
+/// created by the compression/decompression benchmarks.
+///
 /// @param files - vector of files to be deleted.
 static bool clean(const std::vector<std::string_view> &files) {
 	return std::ranges::all_of(files.cbegin(), files.cend(),
